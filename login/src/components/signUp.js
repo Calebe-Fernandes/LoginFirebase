9aeb6450e98d9e1b0308f407e6833380267a9459
CalebeFernandes <calebefernandesps4@gmail.com>
2021-01-22 22:46:42 -0300

    Criando Sign up

---
+++ login/src/components/signUp.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card, Form, Button } from 'react-bootstrap'
+
+
+export default function SignUp(){
+    return(
+        <>
+            <Card>  
+                <Card.Body>
+                    <h2 className = "text-center mb-4">SIGN UP</h2>
+                </Card.Body>
+                <Form>
+                    
+                </Form>
+            </Card>
+            <div className = "w-100 text-center mt-2">
+                 Já possui uma conta? Log in
+            </div>
+          
+        </>
+    )
+}
