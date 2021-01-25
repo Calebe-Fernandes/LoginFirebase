12e4766f142c8e86fab61bedc22df600640c01a1
CalebeFernandes <calebefernandesps4@gmail.com>
2021-01-25 14:59:59 -0300

    Registro de novo usuário

--- login/src/components/signUp.js
+++ login/src/components/signUp.js
@@ -1,20 +1,61 @@
-import React from 'react'
-import { Card, Form, Button } from 'react-bootstrap'
+import React , { useRef, useState} from 'react'
+import { Card, Form, Button, Alert } from 'react-bootstrap'
+import {useAuth} from '../context/authContext'
 
 
 export default function SignUp(){
+
+   const emailRef = useRef()
+   const passwordRef = useRef()
+   const passwordConfirmRef = useRef()
+   const { signup, currentUser }  = useAuth()
+   const [error, setError] = useState("")
+   const [loading, setLoading] = useState(false)
+   
+async function handleSubmit(e){
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value){
+        return setError('Passwords do not match')
+    }
+
+    try{
+        setError('')
+        setLoading(true)
+        await  signup(emailRef.current.value, passwordRef.current.value)
+    }catch{
+        setError('failed to create an account')
+    }
+}
+
     return(
         <>
             <Card>  
                 <Card.Body>
-                    <h2 className = "text-center mb-4">SIGN UP</h2>
+                    <h2 className = "text-center mb-4" >Registre-se</h2>
+                    {error && <Alert variant = 'danger'>{error}</Alert>}
+                    {currentUser && currentUser.email}
+                    <Form className = 'p-4' onSubmit = {handleSubmit} >
+                        <Form.Group id = "email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type= 'email' ref={emailRef}  required/>
+                        </Form.Group>
+
+                        <Form.Group id = "password">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type= 'password' ref={passwordRef}  required/>
+                        </Form.Group>
+
+                        <Form.Group id = "password-confirm">
+                            <Form.Label>Confirme sua senha</Form.Label>
+                            <Form.Control type= 'password' ref={passwordConfirmRef}  required/>
+                        </Form.Group>
+                        <Button  disabled={loading} className = " w-100" type = "submit">Cadastre-se</Button>
+                    </Form>
                 </Card.Body>
-                <Form>
-                    
-                </Form>
             </Card>
             <div className = "w-100 text-center mt-2">
-                 Já possui uma conta? Log in
+                 Já possui uma conta? Entre aqui
             </div>
           
         </>
--- login/src/index.js
+++ login/src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Container} from 'react-bootstrap'
+import {AuthProvider} from './context/authContext'
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+
+    <AuthProvider>
+      <Container className= "d-flex align-items-center justify-content-center "
+        style = {{minHeight: "100vh"}}>
+        <div className="w-100" style = {{maxWidth: '400px'}}>
+          <App />
+        </div>
+      </Container>
+    </AuthProvider>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
